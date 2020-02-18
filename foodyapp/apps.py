45f6d994d42acaf1daf628d37e7f26c59a842f49
Donté Nall <code4zelda@gmail.com>
2020-02-17 23:23:25 -0800

    testing

--- foodyapp/apps.py
+++ foodyapp/apps.py
@@ -3,3 +3,9 @@ from django.apps import AppConfig
 
 class FoodyappConfig(AppConfig):
     name = 'foodyapp'
+
+from django.apps import AppConfig
+
+
+class FoodyappConfig(AppConfig):
+    name = 'foodyapp'
