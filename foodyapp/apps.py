6546a065dd9d87a92e8be773cc7c0e2b5e4d7863
Donté Nall <code4zelda@gmail.com>
2020-02-24 23:34:57 -0800

    removed issue

--- foodyapp/apps.py
+++ foodyapp/apps.py
@@ -4,12 +4,6 @@ from django.apps import AppConfig
 class FoodyappConfig(AppConfig):
     name = 'foodyapp'
 
-from django.apps import AppConfig
-
-
-class FoodyappConfig(AppConfig):
-    name = 'foodyapp'
-
 
 
 
