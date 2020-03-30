beae06730c005197703850a6a10a4a56155d6f93
Donté Nall <code4zelda@gmail.com>
2020-03-29 23:33:22 -0700

    added admin

--- foodyapp/admin.py
+++ foodyapp/admin.py
@@ -10,3 +10,17 @@ admin.site.register(Driver)
 admin.site.register(Meal)
 admin.site.register(Order)
 admin.site.register(OrderDetails)
+
+from django.contrib import admin
+
+
+# Register your models here.
+from foodyapp.models import Restaurant, Customer, Driver, Meal, Order, OrderDetails
+
+admin.site.register(Restaurant)
+admin.site.register(Customer)
+admin.site.register(Driver)
+admin.site.register(Meal)
+admin.site.register(Order)
+admin.site.register(OrderDetails)
+
