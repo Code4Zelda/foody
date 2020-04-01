0ccbd86721d56111ae41a0f64ae23f58a8beb97f
Donté Nall <code4zelda@gmail.com>
2020-04-01 11:31:15 -0700

    removed

--- foodyapp/admin.py
+++ foodyapp/admin.py
@@ -14,13 +14,5 @@ admin.site.register(OrderDetails)
 from django.contrib import admin
 
 
-# Register your models here.
-from foodyapp.models import Restaurant, Customer, Driver, Meal, Order, OrderDetails
 
-admin.site.register(Restaurant)
-admin.site.register(Customer)
-admin.site.register(Driver)
-admin.site.register(Meal)
-admin.site.register(Order)
-admin.site.register(OrderDetails)
 
