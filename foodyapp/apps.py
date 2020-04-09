0adc0873ac5a897d60976c0497e32a122d66f786
Donté Nall <code4zelda@gmail.com>
2020-04-08 23:52:25 -0700

    added app

--- foodyapp/apps.py
+++ foodyapp/apps.py
@@ -4,6 +4,13 @@ from django.apps import AppConfig
 class FoodyappConfig(AppConfig):
     name = 'foodyapp'
 
+    from django.apps import AppConfig
+
+
+class FoodyappConfig(AppConfig):
+    name = 'foodyapp'
+
+
 
 
 
